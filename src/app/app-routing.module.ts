c844a70302f97fb6b691288d7b235bc24a8dcd98
Zone of Development <92488681+ZoneOfDevelopment@users.noreply.github.com>
2022-03-28 00:45:52 +0200

    Add files via upload

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UsersComponent } from './components/users/users.component';
+import { ManageUserComponent } from './components/manage-user/manage-user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'users'},
+  { path: 'users', component: UsersComponent },
+  { path: 'manageuser', component: ManageUserComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
